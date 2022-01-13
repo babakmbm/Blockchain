f403c97f51c1b5524bbbd85c866712a75b3754d9
babakmbm <b7.hosseini@gmail.com>
2022-01-13 18:28:09 +0000

    simple mining block/Get_chain_function

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 blockchain = Blockchain()
 
 
-@app.route('/Mine_A_Block', methods=['GET'])
-def mine_a_block():
+@app.route('/mine_Block', methods=['GET'])
+def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
@@ -21,3 +21,16 @@ def mine_a_block():
                 }
 
     return jsonify(response), 200
+
+
+@app.route('/get_full_chain', methods=['GET'])
+def get_full_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)
+                }
+
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
